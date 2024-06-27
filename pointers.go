220e9a99245062eebb2cf0efa3c0906dcd1ea780
tk1351 <takakikitsui0111@gmail.com>
2024-06-28 07:22:09 +0900

    feat: pointers

--- pointers.go
+++ pointers.go
@@ -6,11 +6,11 @@ func main() {
 	i, j := 42, 2701
 
 	p := &i
-	fmt.Println(*p)
+	fmt.Println(*p) // 42
 	*p = 21
-	fmt.Println(i)
+	fmt.Println(i) // 21
 
 	p = &j
 	*p = *p / 37
-	fmt.Println(j)
+	fmt.Println(j) // 73
 }
