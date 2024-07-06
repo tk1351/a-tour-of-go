2d3de33aa03b11d964c59b44cc31ef5b6252b217
tk1351 <takakikitsui0111@gmail.com>
2024-07-07 07:21:26 +0900

    feat: log 設定

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"a-tour-of-go/utils"
 	"log"
 
 	"gopkg.in/ini.v1"
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
